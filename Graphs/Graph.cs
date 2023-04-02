5c1c87e575796f220debcd5b88249260f1f41778
seeds <seeds@DESKTOP-IL3EESS>
2023-04-02 23:14:47 +0300

    Добавьте файлы проекта.

---
+++ Graphs/Graph.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+
+    using Vertex = System.Int32;
+    internal class Graph
+    {
+        public int[,] num;
+        public Graph()
+        {
+            string[] lines = File.ReadAllLines("Graph_Matrix.txt");
+            num = new int[lines.Length,lines[0].Split(' ').Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] temp =lines[i].Split(' ');
+                for (int j = 0; j < temp.Length; j++)
+                    num[i,j] = Int32.Parse(temp[j]);    
+            }
+           
+            
+        }//Конструктор Graph
+        public void GetGraph()
+        {
+             int p = 1;
+            
+            foreach (var item in num)
+            {
+                if (p++ % num.GetLength(1) == 0)
+                    Console.WriteLine(item);
+                else
+                    Console.Write(item + " ");
+            }
+        }//GetGraph
+        public int weight(Vertex vi, Vertex vj)
+        { 
+            if (num[vi,vj] == 0)
+            {
+                Console.WriteLine("There is no such edge");
+            }
+            return num[vi, vj]; 
+        }
+
+        
+    }
+}
---
+++ Graphs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CommandLine;
+
+namespace Graphs // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var delayOption = new Option<int>
+                (name: "--delay",
+                description: "An option whose argument is parsed as an int.",
+                getDefaultValue: () => 42);
+            var messageOption = new Option<string>
+                ("--message", "An option whose argument is parsed as a string.");
+
+            var rootCommand = new RootCommand();
+            rootCommand.Add(delayOption);
+            rootCommand.Add(messageOption);
+
+            rootCommand.SetHandler((delayOptionValue, messageOptionValue) =>
+            {
+                Console.WriteLine($"--delay = {delayOptionValue}");
+                Console.WriteLine($"--message = {messageOptionValue}");
+            },
+                delayOption, messageOption);
+            //Graph graph = new Graph();
+            //graph.GetGraph();
+            //Console.WriteLine(graph.weight(0, 0));
+
+        }
+
+    }
+}
